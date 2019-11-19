e289d07d5cc0f54084c84f64de325cb6121d4768
cupnoodles <cupn8dles@gmail.com>
2019-11-19 06:03:27 +0300

    Add setup.py, MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kpxch",  # Replace with your own username
+    version="0.0.1",
+    author="cupnoodles",
+    author_email="cupn8dles@gmail.com",
+    description="KeePassXC NaCl CLI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/cupnoodles14/kpxch",
+    packages=setuptools.find_packages(),
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        # "Operating System :: OS Independent",
+        "Topic :: Utilities",
+    ],
+    entry_points={"console_scripts": ["kpxch=kpxch.kpxch:main"]},
+    install_requires=["pysodium"],
+    python_requires=">=3.6",
+)
