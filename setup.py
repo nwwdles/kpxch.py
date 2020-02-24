8e205fdb6af7d17effcd84e7254d09f88b5e08cc
cupnoodles <cupn8dles@gmail.com>
2020-02-24 19:13:04 +0300

    setup: Update description and bump ver

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kpxch",  # Replace with your own username
-    version="0.0.1",
+    version="0.0.3",
     author="cupnoodles",
     author_email="cupn8dles@gmail.com",
-    description="KeePassXC NaCl CLI",
+    description="KeePassXC CLI through NaCl and keepassxc-proxy",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://gitlab.com/cupnoodles14/kpxch",
@@ -17,7 +17,6 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        # "Operating System :: OS Independent",
         "Topic :: Utilities",
     ],
     entry_points={"console_scripts": ["kpxch=kpxch.kpxch:main"]},
